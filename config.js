fa44e27940934c5b602374289698fdf684d05c5e
genixes <genixcourt@gmail.com>
2026-06-25 12:18:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
    place instead of in each file.
    ===================================================================== */
 const firebaseConfig = {
-  apiKey: "AIzaSyByhvlx6i20PrNBWLfjmb4ciG4pfNaMe6Q",
+  apiKey: "AIzaSyC_BWoYyPc9zEddptAjuorROfZ4V7kbxuQ",
   authDomain: "seared-pos-f8248.firebaseapp.com",
   projectId: "seared-pos-f8248",
   storageBucket: "seared-pos-f8248.firebasestorage.app",
